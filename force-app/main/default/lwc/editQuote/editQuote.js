b886e27da1146e059be9870223afa6a0faeb6248
akib <akib@cyntexa.com>
2023-03-24 11:53:16 +0530

    task1

--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -3,14 +3,61 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, api,wire } from "lwc";
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
+import getquote from '@salesforce/apex/QuoteDto.getquote';
+import update_quote from '@salesforce/apex/QuoteDto.upate_quoterecord';
 export default class EditQuote extends LightningElement {
   @api recordId;
-  quoteData = {
-    name: "Quote Name",
-    endDate: 1547250828000
-  };
+  startdate;
+  enddate;
+
+
+  @wire(getquote,{recordId:'$recordId',startdate:'$startdate',endDate:'$enddate'})
+   wiredAccounts({ error, data }) {
+      if (data) {
+          this.startdate = data.Start_Date__c;
+          this.enddate=data.EndDate__c;
+      } else if (error) {
+          console.log(error);
+          this.error = error;
+      }
+  }
+
+
+
+
+  handlechange(event){
+   if(event.target.label=='Start Date'){
+this.startdate=event.target.value;
+
+   }else{
+
+    this.enddate=event.target.value;
+
+   }
+   
+
+  }
+
+
+
+  handlesave(){
+    update_quote({recordId:this.recordId,startdate:this.startdate,endDate:this.enddate}).then(result=>{
+
+      const event = new ShowToastEvent({
+        title: 'Success!',
+        message: 'Record has been successfully upadte!',
+        
+    });
+    this.dispatchEvent(event);
+    
+    })
+
+
+  }
+
 
   renderedCallback() {}
 }
